3e9f46bb26deaea260776ccf4264472c6097762a
Colin Phillips <cphillips@Savannahs-MacBook-Air.local>
2018-06-21 21:29:10 -0700

    Added skeleton for proj2

---
+++ proj2/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int age;
+    char name[20];
+
+    // scanf(<format>, variable's address (&var))
+    // int    -> %d
+    // string -> %s
+
+    // Using printf, ask the user for their age
+    // Using scanf, receive the user's name
+
+    // Using printf, ask the user for their name
+    // Using scanf, receive the user's name
+
+    // Using printf, print out the user's name and age like so:
+    // "Hello. My name is <name> and I am <age> years old."
+}
